20f6fc8f49debf2dfa00ac2854ee33e6e91dfcf0
ExiledGundam <mohd.farhan@vltkl.com>
2014-04-28 15:47:10 +0800

    change credential for local
    Change db name and password

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wpgit');
 
 /** MySQL database username */
-define('DB_USER', 'localhost');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'integricityb!qwerty');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
