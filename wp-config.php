56e1fe66be204bd7eab34503b3a76e6bfd85f7dd
ExiledGundam <mohd.farhan@vltkl.com>
2014-04-28 15:44:32 +0800

    Change db credentials
    For staging

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wpgit');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'localhost');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'integricityb!qwerty');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
